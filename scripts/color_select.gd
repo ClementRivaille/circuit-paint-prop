9771198e62695f4aea9a321416cf4a39bff6d330
Clément Rivaille <rivaille.clement@orange.fr>
2024-12-15 19:51:50 +0100

    feat(color_pick): show selected color

--- scripts/color_select.gd
+++ scripts/color_select.gd
@@ -1,6 +1,8 @@
 extends Control
 class_name ColorSelect
 
+@export var outline_color := Color.WHITE
+@export var outline_color_selected := Color.BLACK
 @export var color: Color
 @export var icon: Texture2D
 @export var text := ""
@@ -9,6 +11,7 @@ class_name ColorSelect
 @onready var fill: TextureRect = $Button/Fill
 @onready var icon_display: TextureRect = $Icon
 @onready var label: Label = $Label
+@onready var outline: Control = $Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,5 +19,11 @@ func _ready() -> void:
 	icon_display.texture = icon
 	label.text = text
 
+	GameStore.color_changed.connect(update_selection)
+	update_selection(GameStore.selected_color)
+
 func on_click():
 	GameStore.set_selected_color(tile)
+
+func update_selection(selected: Vector2i):
+	outline.self_modulate = outline_color_selected if selected == tile else outline_color
