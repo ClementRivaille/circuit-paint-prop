c2bc8b2aaf6cf39c21acac8decea56528def37f1
Clément Rivaille <rivaille.clement@orange.fr>
2024-12-15 11:55:23 +0100

    feat(items): place items with level

--- scripts/items_placement.gd
+++ scripts/items_placement.gd
@@ -10,18 +10,22 @@ class_name ItemsPlacement
 @onready var checkpoints: Array[TrackItem] = []
 
 func _ready() -> void:
-	init_canva()
 	GameStore.mode_changed.connect(on_mode_change)
 	GameStore.start_validated.connect(set_start_validation)
 	GameStore.goal_validated.connect(set_goal_validation)
 	GameStore.checkpoint_validated.connect(set_checkpoint_validation)
+	GameStore.level_begin.connect(init_canva)
 
-func init_canva():
+func init_canva(level: Level):
 	# place start item
-	start_item.position = selection_target.position
+	if (level.locked_start):
+		var global_start_pos := GameStore.get_global_position(level.start_position)
+		start_item.position = to_local(global_start_pos)
+	else:
+		start_item.position = selection_target.position
 	GameStore.drop_item(Store.TrackItemType.START, GameStore.get_position_on_map(start_item.global_position))
 
-	var grid_pos := Vector2(1,0)
+	var grid_pos := Vector2(0 if level.locked_start else 1,0)
 	# generate checkpoins
 	var checkpoints_pos: Array[Vector2i] = []
 	for idx in range(0, GameStore.total_checkpoints):
@@ -29,23 +33,30 @@ func init_canva():
 		checkpoint_item.type = Store.TrackItemType.CHECKPOINT
 		checkpoint_item.index = idx
 
-		var grid_placement := selection_target.position + grid_pos * cell_size
-		checkpoint_item.position = grid_placement
+		if level.checkpoints_positions.size() > idx:
+			var global_pos := GameStore.get_global_position(level.checkpoints_positions[idx])
+			checkpoint_item.position = to_local(global_pos)
+		else:
+			var grid_placement := selection_target.position + grid_pos * cell_size
+			checkpoint_item.position = grid_placement
+			if grid_pos.x == 0:
+				grid_pos.x = 1
+			else:
+				grid_pos.x = 0
+				grid_pos.y += 1
 
 		add_child(checkpoint_item)
 		checkpoints.append(checkpoint_item)
 		checkpoints_pos.append(GameStore.get_position_on_map(checkpoint_item.global_position))
 
-		if grid_pos.x == 0:
-			grid_pos.x = 1
-		else:
-			grid_pos.x = 0
-			grid_pos.y += 1
-
 	GameStore.generate_checkpoints(checkpoints_pos)
 
 	# place goal item
-	goal_item.position = selection_target.position + grid_pos * cell_size
+	if level.locked_goal:
+		var global_goal_pos := GameStore.get_global_position(level.goal_position)
+		goal_item.position = to_local(global_goal_pos)
+	else:
+		goal_item.position = selection_target.position + grid_pos * cell_size
 	GameStore.drop_item(Store.TrackItemType.GOAL, GameStore.get_position_on_map(goal_item.global_position))
 
 func on_mode_change(mode: Store.GameMode):
