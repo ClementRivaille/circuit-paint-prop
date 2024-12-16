570b1df31adb9ee4761293fd93c510d7a1a7de88
Clément Rivaille <rivaille.clement@orange.fr>
2024-12-16 14:37:51 +0100

    feat(music): title & results

--- scripts/music.gd
+++ scripts/music.gd
@@ -2,6 +2,13 @@ extends AudioStreamPlayer
 
 var playback: AudioStreamPlaybackInteractive
 
+var CLIP_MAPPING: Dictionary = {
+	Store.GameMode.RACING: "race",
+	Store.GameMode.PAINTING: "paint",
+	Store.GameMode.TITLE: "title",
+	Store.GameMode.RESULTS: "results"
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	play()
@@ -9,7 +16,5 @@ func _ready() -> void:
 	GameStore.mode_changed.connect(set_track)
 
 func set_track(mode: Store.GameMode):
-	if mode == Store.GameMode.RACING:
-		playback.switch_to_clip_by_name("race")
-	elif mode == Store.GameMode.PAINTING:
-		playback.switch_to_clip_by_name("paint")
+	var clip_name: String = CLIP_MAPPING[mode]
+	playback.switch_to_clip_by_name(clip_name)
