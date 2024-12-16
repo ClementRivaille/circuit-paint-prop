9a3d032772f5119db438e9bc6cb96a71af022189
Clément Rivaille <rivaille.clement@orange.fr>
2024-12-16 12:30:11 +0100

    feat: transitions between scenes

--- scripts/transition_layer.gd
+++ scripts/transition_layer.gd
@@ -3,32 +3,40 @@ extends ColorRect
 @export var transition_duration := 0.3
 @export var in_out_delay := 0.2
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GameStore.mode_transision.connect(horizontal_trans)
+	GameStore.mode_transision.connect(on_mode_transition)
 	init_anchors()
 
-func init_anchors():
-	set_anchor(SIDE_TOP, 0.0, true)
+func init_anchors(vertical := false):
+	set_anchor(SIDE_TOP, 1.0 if vertical else 0.0, true)
 	set_anchor(SIDE_BOTTOM, 1.0, true)
 	set_anchor(SIDE_LEFT, 0.0, true)
-	set_anchor(SIDE_RIGHT, 0.0, true)
+	set_anchor(SIDE_RIGHT, 1.0 if vertical else 0.0, true)
 
-func horizontal_trans(_from, _to):
-	init_anchors()
+func animate_transition(vertical := false):
+	init_anchors(vertical)
 
+	var from: float = 1.0 if vertical else 0.0
+	var to: float = 0.0 if vertical else 1.0
+
+	var moved_side: Side = SIDE_TOP if vertical else SIDE_RIGHT
 	var tween = create_tween()
 	tween.tween_method(func (value: float):
-		set_anchor(SIDE_RIGHT, value, true),
-		0.0, 1.0, transition_duration
+		set_anchor(moved_side, value, true),
+		from, to, transition_duration
 		).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
 	await get_tree().create_timer(transition_duration).timeout
 
 	await get_tree().create_timer(in_out_delay).timeout
 
+	moved_side = SIDE_BOTTOM if vertical else SIDE_LEFT
 	tween = create_tween()
 	tween.tween_method(func (value: float):
-		set_anchor(SIDE_LEFT, value, true),
-		0.0, 1.0, transition_duration
+		set_anchor(moved_side, value, true),
+		from, to, transition_duration
 		).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+
+func on_mode_transition(_from: Store.GameMode, to: Store.GameMode):
+	var vertical := to == Store.GameMode.RACING || to == Store.GameMode.RESULTS
+	animate_transition(vertical)
